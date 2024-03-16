79888a6721a4aa1a251208ed723788e2ecdb184a
shihabG6 <shihabuddin0194@gmail.com>
2024-03-16 14:56:34 +0600

    added visit country list

--- src/component/Countri/Countri.jsx
+++ src/component/Countri/Countri.jsx
@@ -1,30 +1,35 @@
 import { useState } from "react";
 import "./countri.css";
-const Countri = ({ Countri }) => {
-  const { name, flags, maps,population,area} = Countri;
+const Countri = ({ Countri ,handelCountryCount}) => {
+  const { name,cca2, flags, maps,population,area} = Countri;
 
 
-  console.log(Countri);
+  // console.log(Countri);
   const[visite, setVisite]=useState(false);
   const handelVisite = ()=>{
     setVisite(!visite)
   }
 
-  const handelMap =()=>{
-    window.open(maps.googleMaps)
-  }
+  // const handelMap =()=>{
+  //   window.open(maps.googleMaps)
+  // }
 
-
+  // console.log(handelCountryCount)
+  // const dd = ()=>{
+  //   handelCountryCount(Countri)
+  // }
 
   return (
-    <div className="countri">
+    <div className={`countri ${visite&& "bg"}`}>
       <img src={flags?.png} alt="" />
-      <h2>{name?.common}</h2>
+      <h2> {name?.common}</h2>
       <p>Populasion:{population}</p>
       <p>Area: {area} m<sup>2</sup></p>
+      <p>Code:{cca2}</p>
+      <button onClick={()=>handelCountryCount(Countri)}>Visited Country</button>
       <button onClick={handelVisite}>{visite? 'Vsite':'Going'}</button>
       <p>{visite?'this country visited':'this is not visted'}</p>
-      <button onClick={handelMap}> see on google map</button>
+      {/* <button onClick={handelMap}> see on google map</button> */}
     </div>
   );
 };
--- src/component/Countries/Countries.jsx
+++ src/component/Countries/Countries.jsx
@@ -1,22 +1,38 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Countri from "../Countri/Countri";
-import './countries.css'
+import "./countries.css";
 
 const Countries = () => {
-  const [countries,setCountrics]=useState([]);
+  const [countries, setCountrics] = useState([]);
+  const [visitedCountry, setCountryCount] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
-    .then(res => res.json())
-    .then(data => setCountrics(data))
-  },[])
-    
+      .then((res) => res.json())
+      .then((data) => setCountrics(data));
+  }, []);
+
+  const handelCountryCount = (country) => {
+    const newVisit = [...visitedCountry, country];
+    setCountryCount(newVisit);
+  };
+
   return (
-    <div className="countries">
-      {
-        countries.map((countri) =><Countri Countri={countri} key={countri.cca2}></Countri>)
-      }
+    <div>
+      <h2>Vsite:{visitedCountry.length}</h2>
+      {visitedCountry.map((country) => (
+        <li>{country.name.common}</li>
+      ))}
+      <div className="countries">
+        {countries.map((countri) => (
+          <Countri
+            Countri={countri}
+            handelCountryCount={handelCountryCount}
+            key={countri.cca2}
+          ></Countri>
+        ))}
+      </div>
     </div>
   );
 };
